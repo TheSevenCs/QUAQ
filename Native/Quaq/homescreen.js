6b52d7ac9a201148b3d946920cca9a268fb29a85
Yuval <117393676+TheSevenCs@users.noreply.github.com>
2024-07-22 22:01:18 -0400

    homescreen file

---
+++ Native/Quaq/homescreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function App() {
+  return (
+      // markup code here
+  );
+}
+
+const styles = StyleSheet.create({
+  // stylesheet
+});
