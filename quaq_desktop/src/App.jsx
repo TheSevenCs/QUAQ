0794596fe4e26fbd91865e8c758d4d8e4d17df1c
Andrew Lee <leeandrew135@gmail.com>
2024-08-10 02:59:33 -0400

    setup for desktop

---
+++ quaq_desktop/src/App.jsx
@@ -0,0 +1,13 @@
+// React/App.jsx
+import React from "react";
+import Clients from "./components/pages/pageClients.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <Clients />
+    </div>
+  );
+};
+
+export default App;
---
+++ quaq_desktop/src/components/pages/pageClients.jsx
@@ -0,0 +1,373 @@
+// React/components/pages/Clients.jsx
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+// const generalModule = require("/DataAccess/database.js");
+
+const Clients = () => {
+  const [clients, setClients] = useState([]);
+  const [newClient, setNewClient] = useState({
+    company_id: "",
+    clientFirstName: "",
+    clientLastName: "",
+    clientEmail: "",
+    clientPhone: "",
+    clientDate: "",
+    clientActive: true,
+    clientWebsite: "",
+    clientAddress: "",
+  });
+  const [editClient, setEditClient] = useState({
+    client_id: "",
+    company_id: "",
+    clientFirstName: "",
+    clientLastName: "",
+    clientEmail: "",
+    clientPhone: "",
+    clientDate: "",
+    clientActive: true,
+    clientWebsite: "",
+    clientAddress: "",
+  });
+  const [deleteClient_id, setDeleteClient_id] = useState({
+    client_id: "",
+    company_id: "",
+  });
+
+  useEffect(() => {
+    getClients();
+  }, []);
+
+  // CREATE
+  const addClient = async () => {
+    console.log("ENTERING addClient().");
+    try {
+      const response = await axios.post(
+        "http://192.168.0.50:5500/clients",
+        {},
+        {
+          params: {
+            client_id: Math.random().toString(36).substr(2, 9), // Temporary ID for testing
+            company_id: newClient.company_id,
+            newFirstName: newClient.clientFirstName,
+            newLastName: newClient.clientLastName,
+            newEmail: newClient.clientEmail,
+            newPhone: newClient.clientPhone,
+            newDate: newClient.clientDate,
+            newClientActive: newClient.clientActive,
+            newWebsite: newClient.clientWebsite,
+            newAddress: newClient.clientAddress,
+          },
+        }
+      );
+
+      // LOADS NEW DATA THEN RESETS ADD FIELDS
+      getClients();
+      setNewClient({
+        company_id: "",
+        clientFirstName: "",
+        clientLastName: "",
+        clientEmail: "",
+        clientPhone: "",
+        clientDate: "",
+        clientActive: true,
+        clientWebsite: "",
+        clientAddress: "",
+      });
+      console.log("FROM pageClients.jsx, Client ADDED SUCCESSFULLY:", response);
+    } catch (error) {
+      console.error("FROM pageCLients.jsx, ERROR ADDING Client:", error);
+    }
+  };
+  // READ
+  const getClients = async () => {
+    console.log("FROM getClients.");
+    try {
+      const response = await axios.get("http://192.168.0.50:5500/clients");
+      setClients(response.data);
+      // console.log("response.data: ", response.data); // TESTING
+      // console.log("response: ", response); // TESTING
+    } catch (error) {
+      console.error("Error fetching clients:", error);
+    }
+  };
+  // UPDATE
+  const updateClient = async () => {
+    try {
+      const response = await axios.patch("http://192.168.0.50:5500/clients", {
+        params: {
+          client_id: editClient.client_id,
+          company_id: editClient.company_id,
+          clientFirstName: editClient.clientFirstName,
+          clientLastName: editClient.clientLastName,
+          clientEmail: editClient.clientEmail,
+          clientPhone: editClient.clientPhone,
+          clientDate: editClient.clientDate,
+          clientActive: editClient.clientActive,
+          clientWebsite: editClient.clientWebsite,
+          clientAddress: editClient.clientAddress,
+        },
+      });
+
+      // LOADS NEW DATA THEN RESETS FIELDS
+      getClients();
+      setEditClient({
+        client_id: "",
+        company_id: "",
+        clientFirstName: "",
+        clientLastName: "",
+        clientEmail: "",
+        clientPhone: "",
+        clientDate: "",
+        clientActive: true, // Setting this to null causes error with toString()
+        clientWebsite: "",
+        clientAddress: "",
+      });
+
+      console.log("FROM pageClients.jsx, Client EDITED:", response);
+    } catch (error) {
+      console.log("FROM pageClients.jsx, ERROR EDITING Client:", error);
+    }
+  };
+  // DELETE
+  const deleteClient = async () => {
+    try {
+      const response = await axios.delete("http://192.168.0.50:5500/clients", {
+        params: {
+          client_id: deleteClient_id.client_id,
+          company_id: deleteClient_id.company_id,
+        },
+      });
+
+      // LOADS NEW DATA THEN RESETS FIELDS
+      getClients();
+      setDeleteClient_id({
+        client_id: "",
+        company_id: "",
+        clientFirstName: "",
+        clientLastName: "",
+        clientEmail: "",
+        clientPhone: "",
+        clientDate: "",
+        clientActive: null,
+        clientWebsite: "",
+        clientAddress: "",
+      });
+      console.log("FROM pageClients.jsx, Client DELETED: ", response);
+    } catch (error) {
+      console.log("FROM pageClients.jsx, ERROR DELETING Client: ", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>CLIENTS PAGE</h1>
+
+      {/* ADD CLIENT */}
+      <div>
+        <h2>ADD NEW CLIENT</h2>
+        <input
+          type="text"
+          placeholder="company_id"
+          value={newClient.company_id}
+          onChange={(e) =>
+            setNewClient({ ...newClient, company_id: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          placeholder="First Name"
+          value={newClient.clientFirstName}
+          onChange={(e) =>
+            setNewClient({ ...newClient, clientFirstName: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={newClient.clientLastName}
+          onChange={(e) =>
+            setNewClient({ ...newClient, clientLastName: e.target.value })
+          }
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={newClient.clientEmail}
+          onChange={(e) =>
+            setNewClient({ ...newClient, clientEmail: e.target.value })
+          }
+        />
+        <input
+          type="tel"
+          placeholder="Phone"
+          value={newClient.clientPhone}
+          onChange={(e) =>
+            setNewClient({ ...newClient, clientPhone: e.target.value })
+          }
+        />
+        <input
+          type="date"
+          placeholder="Date"
+          value={newClient.clientDate}
+          onChange={(e) =>
+            setNewClient({ ...newClient, clientDate: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          placeholder="Website"
+          value={newClient.clientWebsite}
+          onChange={(e) =>
+            setNewClient({ ...newClient, clientWebsite: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          placeholder="Address"
+          value={newClient.clientAddress}
+          onChange={(e) =>
+            setNewClient({ ...newClient, clientAddress: e.target.value })
+          }
+        />
+        <button onClick={addClient}>ADD CLIENT</button>
+      </div>
+
+      {/* LOAD CLIENT */}
+      <div>
+        <h2>LOAD BUTTON</h2>
+        <button onClick={getClients}>GET CLIENTS</button>
+      </div>
+
+      {/* UPDATE CLIENT */}
+      <div>
+        <h2>UPDATE CLIENT</h2>
+        <input
+          type="text"
+          placeholder="client_id"
+          value={editClient.client_id}
+          onChange={(e) =>
+            setEditClient({ ...editClient, client_id: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          placeholder="company_id"
+          value={editClient.company_id}
+          onChange={(e) =>
+            setEditClient({ ...editClient, company_id: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          placeholder="First Name"
+          value={editClient.clientFirstName}
+          onChange={(e) =>
+            setEditClient({ ...editClient, clientFirstName: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={editClient.clientLastName}
+          onChange={(e) =>
+            setEditClient({ ...editClient, clientLastName: e.target.value })
+          }
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={editClient.clientEmail}
+          onChange={(e) =>
+            setEditClient({ ...editClient, clientEmail: e.target.value })
+          }
+        />
+        <input
+          type="tel"
+          placeholder="Phone"
+          value={editClient.clientPhone}
+          onChange={(e) =>
+            setEditClient({ ...editClient, clientPhone: e.target.value })
+          }
+        />
+        <input
+          type="date"
+          placeholder="Date"
+          value={editClient.clientDate}
+          onChange={(e) =>
+            setEditClient({ ...editClient, clientDate: e.target.value })
+          }
+        />
+        <select
+          name="clientActive"
+          value={editClient.clientActive.toString()}
+          onChange={(e) =>
+            setEditClient({ ...editClient, clientActive: e.target.value })
+          }
+        >
+          <option value={true}>Active/True</option>
+          <option value={false}>Inactive/False</option>
+        </select>
+        <input
+          type="text"
+          placeholder="Website"
+          value={editClient.clientWebsite}
+          onChange={(e) =>
+            setEditClient({ ...editClient, clientWebsite: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          placeholder="Address"
+          value={editClient.clientAddress}
+          onChange={(e) =>
+            setEditClient({ ...editClient, clientAddress: e.target.value })
+          }
+        />
+        <button onClick={updateClient}>UPDATE CLIENT</button>
+      </div>
+
+      {/* DELETE CLIENT */}
+      <div>
+        <h2>DELETE BY CLIENT ID</h2>
+        <input
+          type="text"
+          placeholder="DELETE CLIENT ID"
+          value={deleteClient_id.client_id}
+          onChange={(e) =>
+            setDeleteClient_id({
+              ...deleteClient_id,
+              client_id: e.target.value,
+            })
+          }
+        />
+        <input
+          type="text"
+          placeholder="DELETE COMPANY ID"
+          value={deleteClient_id.company_id}
+          onChange={(e) =>
+            setDeleteClient_id({
+              ...deleteClient_id,
+              company_id: e.target.value,
+            })
+          }
+        />
+        <button onClick={deleteClient}>DELETE CLIENT</button>
+      </div>
+
+      <h2>Client List</h2>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.client_id}>
+            {Object.entries(client).map(([key, value]) => (
+              <span key={key}>
+                {key}: {value}{" "}
+              </span>
+            ))}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Clients;
