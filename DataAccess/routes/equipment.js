0387e260d87e3ae10df1cfc88b25c87ecfcd4ab0
Lev5942 <145793928+Lev5942@users.noreply.github.com>
2024-08-12 17:16:47 -0400

    Update equipment and equipmentGroup

--- DataAccess/routes/equipment.js
+++ DataAccess/routes/equipment.js
@@ -2,6 +2,165 @@ const express = require("express");
 const generalModule = require("../database.js");
 const router = express.Router();
 
-//
+// CREATE
+router.post("/", async (req, res) => {
+  const {
+    equipmentGroup_id,
+    job_id = "0", // default value 0
+    equipmentName,
+    equipmentSerialNumber,
+    equipmentLocation,
+    equipmentStatus, // available/in_use/maintenance
+    purchaseDate, // YYYY-MM-DD
+    lastServiceDate, // YYYY-MM-DD
+  } = req.body;
+
+  const newEquipment = {
+    equipment_id: (
+      await generalModule.TEMPgenerateID(9000000000000, 9999999999999)
+    ).toString(),
+    equipmentGroup_id,
+    job_id,
+    equipmentName,
+    equipmentSerialNumber,
+    equipmentLocation,
+    equipmentStatus,
+    equipmentPurchaseDate: purchaseDate,
+    equipmentLastServiceDate: lastServiceDate,
+  };
+
+  try {
+    // Ensure equipmentName is unique
+    const existingEquipment = await generalModule.getItemByField(
+      "Equipment",
+      "equipmentName",
+      equipmentName
+    );
+    if (existingEquipment) {
+      return res.status(400).json({ error: "Equipment name must be unique" });
+    }
+
+    const result = await generalModule.addToDatabase("Equipment", newEquipment);
+    console.log("FROM equipment.js, NEW Equipment ADDED: ", result);
+    res.status(201).json({ message: "Equipment created successfully", result });
+  } catch (error) {
+    console.error("FROM equipment.js, ERROR CREATING NEW Equipment: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// READ
+router.get("/", async (req, res) => {
+  try {
+    const equipment = await generalModule.getFromDatabase("Equipment");
+    console.log("FROM equipment.js, RESULTS FROM Equipment TABLE: ", equipment);
+
+    const formattedEquipment = equipment.map((item) => ({
+      equipment_id: item.equipment_id,
+      equipmentGroup_id: item.equipmentGroup_id,
+      job_id: item.job_id,
+      equipmentName: item.equipmentName,
+      equipmentSerialNumber: item.equipmentSerialNumber,
+      equipmentLocation: item.equipmentLocation,
+      equipmentStatus: item.equipmentStatus,
+      equipmentPurchaseDate: item.equipmentPurchaseDate,
+      equipmentLastServiceDate: item.equipmentLastServiceDate,
+    }));
+
+    res.status(200).json(formattedEquipment);
+  } catch (error) {
+    console.error("FROM equipment.js, ERROR GETTING Equipment: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// UPDATE
+router.patch("/", async (req, res) => {
+  const {
+    equipment_id,
+    equipmentGroup_id,
+    editedJobId = "0", // default value 0
+    editedName,
+    editedSerialNumber,
+    editedLocation,
+    editedStatus,
+    editedPurchaseDate, // YYYY-MM-DD
+    editedLastServiceDate, // YYYY-MM-DD
+  } = req.body;
+
+  const equipmentDetails = {
+    equipment_id,
+    equipmentGroup_id,
+    job_id: editedJobId,
+    equipmentName: editedName,
+    equipmentSerialNumber: editedSerialNumber,
+    equipmentLocation: editedLocation,
+    equipmentStatus: editedStatus,
+    equipmentPurchaseDate: editedPurchaseDate,
+    equipmentLastServiceDate: editedLastServiceDate,
+  };
+
+  const updateExpression =
+    "SET " +
+    Object.keys(equipmentDetails)
+      .map((key) => `${key} = :${key}`)
+      .join(", ");
+
+  const expressionAttributeValues = {};
+  Object.entries(equipmentDetails).forEach(([key, value]) => {
+    expressionAttributeValues[`:${key}`] = { S: value };
+  });
+
+  const primaryKey = {
+    equipment_id: { S: equipment_id },
+  };
+
+  try {
+    // Ensure equipmentName is unique
+    const existingEquipment = await generalModule.getItemByField(
+      "Equipment",
+      "equipmentName",
+      editedName
+    );
+    if (existingEquipment && existingEquipment.equipment_id !== equipment_id) {
+      return res.status(400).json({ error: "Equipment name must be unique" });
+    }
+
+    const response = await generalModule.updateItemInDatabase(
+      "Equipment",
+      primaryKey,
+      updateExpression,
+      expressionAttributeValues
+    );
+
+    console.log("FROM equipment.js, Equipment EDITED: ", response);
+    res
+      .status(200)
+      .json({ message: "Equipment updated successfully", response });
+  } catch (error) {
+    console.error("FROM equipment.js, ERROR EDITING Equipment: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// DELETE
+router.delete("/", async (req, res) => {
+  const { equipment_id } = req.body;
+
+  try {
+    const response = await generalModule.deleteFromDatabase(
+      "Equipment",
+      equipment_id
+    );
+    console.log(
+      `FROM equipment.js, Equipment: ${equipment_id} DELETED: `,
+      response
+    );
+    res.status(204).json({ message: "Equipment deleted successfully" });
+  } catch (error) {
+    console.error("FROM equipment.js, Equipment COULD NOT BE DELETED: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 module.exports = router;
--- DataAccess/routes/equipmentGroups.js
+++ DataAccess/routes/equipmentGroups.js
@@ -2,6 +2,145 @@ const express = require("express");
 const generalModule = require("../database.js");
 const router = express.Router();
 
-//
+// CREATE
+router.post("/", async (req, res) => {
+  const {
+    company_id,
+    groupName,
+    groupDescription,
+    groupDate, // YYYY-MM-DD
+  } = req.body;
+
+  const newGroup = {
+    equipmentGroup_id: (
+      await generalModule.TEMPgenerateID(9000000000000, 9999999999999)
+    ).toString(),
+    company_id,
+    equipmentGroupName: groupName,
+    equipmentGroupDescription: groupDescription,
+    equipmentGroupDate: groupDate,
+  };
+
+  try {
+    const result = await generalModule.addToDatabase(
+      "EquipmentGroups",
+      newGroup
+    );
+    console.log("FROM equipmentGroups.js, NEW Equipment Group ADDED: ", result);
+    res
+      .status(201)
+      .json({ message: "Equipment Group created successfully", result });
+  } catch (error) {
+    console.error(
+      "FROM equipmentGroups.js, ERROR CREATING NEW Equipment Group: ",
+      error
+    );
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// READ
+router.get("/", async (req, res) => {
+  try {
+    const groups = await generalModule.getFromDatabase("EquipmentGroups");
+    console.log(
+      "FROM equipmentGroups.js, RESULTS FROM EquipmentGroups TABLE: ",
+      groups
+    );
+
+    const formattedGroups = groups.map((group) => ({
+      equipmentGroup_id: group.equipmentGroup_id,
+      company_id: group.company_id,
+      equipmentGroupName: group.equipmentGroupName,
+      equipmentGroupDescription: group.equipmentGroupDescription,
+      equipmentGroupDate: group.equipmentGroupDate,
+    }));
+
+    res.status(200).json(formattedGroups);
+  } catch (error) {
+    console.error(
+      "FROM equipmentGroups.js, ERROR GETTING Equipment Groups: ",
+      error
+    );
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// UPDATE
+router.patch("/", async (req, res) => {
+  const {
+    group_id,
+    company_id,
+    editedName,
+    editedDescription,
+    editedDate, // YYYY-MM-DD
+  } = req.body;
+
+  const groupDetails = {
+    equipmentGroup_id: group_id,
+    company_id,
+    equipmentGroupName: editedName,
+    equipmentGroupDescription: editedDescription,
+    equipmentGroupDate: editedDate,
+  };
+
+  const updateExpression =
+    "SET " +
+    Object.keys(groupDetails)
+      .map((key) => `${key} = :${key}`)
+      .join(", ");
+
+  const expressionAttributeValues = {};
+  Object.entries(groupDetails).forEach(([key, value]) => {
+    expressionAttributeValues[`:${key}`] = { S: value };
+  });
+
+  const primaryKey = {
+    equipmentGroup_id: { S: group_id },
+  };
+
+  try {
+    const response = await generalModule.updateItemInDatabase(
+      "EquipmentGroups",
+      primaryKey,
+      updateExpression,
+      expressionAttributeValues
+    );
+
+    console.log("FROM equipmentGroups.js, Equipment Group EDITED: ", response);
+    res
+      .status(200)
+      .json({ message: "Equipment Group updated successfully", response });
+  } catch (error) {
+    console.error(
+      "FROM equipmentGroups.js, ERROR EDITING Equipment Group: ",
+      error
+    );
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// DELETE
+router.delete("/", async (req, res) => {
+  const { group_id } = req.body;
+
+  try {
+    const response = await generalModule.deleteFromDatabase(
+      "EquipmentGroups",
+      group_id
+    );
+    console.log(
+      `FROM equipmentGroups.js, Equipment Group: ${group_id} DELETED: `,
+      response
+    );
+    res.status(204).json({ message: "Equipment Group deleted successfully" });
+  } catch (error) {
+    console.error(
+      "FROM equipmentGroups.js, Equipment Group COULD NOT BE DELETED: ",
+      error
+    );
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 module.exports = router;
