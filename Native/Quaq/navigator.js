681846f59f8bae51bff451ac49de46ce244f6e7a
Andrew Lee <leeandrew135@gmail.com>
2024-08-25 16:54:06 -0400

    updated navigator for testing

--- Native/Quaq/navigator.js
+++ Native/Quaq/navigator.js
@@ -1,14 +1,21 @@
-import React from "react";
+// import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { ScreenHome, ScreenClients } from "./screens";
+import { ScreenHome, ScreenClients, ScreenPersonalSchedule } from "./screens";
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator
+      initialRouteName="PersonalSchedule"
+      screenOptions={{ headerShown: false }} // Hides default ugly header banner
+    >
       <Stack.Screen name="Home" component={ScreenHome} />
       <Stack.Screen name="Clients" component={ScreenClients} />
+      <Stack.Screen
+        name="PersonalSchedule"
+        component={ScreenPersonalSchedule}
+      />
     </Stack.Navigator>
   );
 };
