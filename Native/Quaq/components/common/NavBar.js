151571c02814f78c96cf8da81389a019f7d4036a
Andrew Lee <leeandrew135@gmail.com>
2024-09-23 18:41:44 -0400

    added try catch and updated styles

--- Native/Quaq/components/common/NavBar.js
+++ Native/Quaq/components/common/NavBar.js
@@ -2,14 +2,18 @@ import React from "react";
 import { View, Text, StyleSheet, Dimensions } from "react-native";
 
 const NavBar = ({ screenTitle }) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.titleBar}>
-        <Text style={styles.titleText}>{screenTitle}</Text>
+  try {
+    return (
+      <View style={styles.container}>
+        <View style={styles.titleBar}>
+          <Text style={styles.titleText}>{screenTitle}</Text>
+        </View>
+        <View style={styles.line} />
       </View>
-      <View style={styles.line} />
-    </View>
-  );
+    );
+  } catch (error) {
+    console.log("FROM NavBar.js:", error);
+  }
 };
 
 const { height: windowHeight } = Dimensions.get("window");
@@ -17,12 +21,13 @@ const { height: windowHeight } = Dimensions.get("window");
 const styles = StyleSheet.create({
   container: {
     width: "100%",
-    height: windowHeight * 0.1 + 2,
+    // height: windowHeight * 0.1 + 2,
     backgroundColor: "transparent",
+    flex: 1,
   },
   titleBar: {
     width: "100%",
-    height: windowHeight * 0.1,
+    height: "100%",
     backgroundColor: "black",
     alignItems: "center", // horizontal move to center
     justifyContent: "center", // vertical move to center
@@ -31,11 +36,15 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 33,
     fontFamily: "Selawik-Semilight",
+    justifyContent: "center",
+    alignContent: "center",
   },
   line: {
     height: 2,
     backgroundColor: "#D9AC6E",
     width: "100%",
+    position: "absolute",
+    bottom: 0,
   },
 });
 
