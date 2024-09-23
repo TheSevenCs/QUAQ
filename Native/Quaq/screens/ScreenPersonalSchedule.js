2d721faa3116f6d546b67abbd5eae70e1072bdaf
Andrew Lee <leeandrew135@gmail.com>
2024-09-23 18:40:07 -0400

    dynamically loads elements based on the axios get call

--- Native/Quaq/screens/ScreenPersonalSchedule.js
+++ Native/Quaq/screens/ScreenPersonalSchedule.js
@@ -1,16 +1,18 @@
-import React, { useState, useCallback } from "react";
+// IMPORT REACT
+import React, { useState, useEffect, useCallback } from "react";
 import {
   View,
   Text,
   StyleSheet,
-  TouchableOpacity,
   StatusBar,
   Image,
+  ActivityIndicator,
+  Dimensions,
 } from "react-native";
 import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
+// IMPORT COMMON STYLES
 import styleCommon from "../styles/styleCommon";
-// import { RadialGradient } from "react-native-gradients";
 
 // IMPORT COMPONENTS
 import {
@@ -20,95 +22,155 @@ import {
   JobEntry,
 } from "../components/personalSchedule";
 import { NavBar, BackgroundGradient } from "../components/common";
+import CustomButton from "../components/CustomButton";
+
+// IMPORT AXIOS
+import axios from "axios";
 
 const ScreenPersonalSchedule = () => {
+  console.log("ScreenPersonalSchedule.js ENTERED.");
+
   const navigation = useNavigation();
 
+  // NETOWRKING CONSTANTS
   const network_ip = "http://localhost";
   const router_port = ":5500";
 
   // STATE VARIABLES
-  const [clients, setClients] = useState([]);
+  const [groupedJobsData, setGroupedJobsData] = useState([]); // TRACK JOBS DATA
+  const [isLoading, setIsLoading] = useState(true); // TRACK PAGE LOADING STATE
 
-  // Automatically called when the component is loaded
-  useEffect(() => {
-    getClients();
-  }, []);
-
-  const testerDate1 = "2024/08/23";
-  const testerDate2 = "2024/08/25";
+  // TESTING
+  const testerDate1 = "2024/08/25";
+  const testerDate2 = "2024/08/31";
   const testerDateRange1 = "2024/08/23 - 2024/08/23";
 
-  // READ
-  const getClients = async () => {
-    console.log("FROM ScreenPersonalSchedule."); // TESTING
+  // Automatically called when component is loaded
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetchAndGroupJobs(); // Call async function
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log("UPDATED GROUPED JOBS", groupedJobsData);
+  }, [groupedJobsData]);
+
+  // DATABSE CALL, return: null
+  // Takes the jobsData which is formatted as an array of objects,
+  // and converts it to a dictionary where the dates are the keys.
+  // return: null and setGroupedJobsData(groupedJobs)
+  const fetchAndGroupJobs = async () => {
+    setIsLoading(true); // LOADING ICON STATE
+
     try {
+      // response contains all elems of the request; config, data, status, ...
       const response = await axios.get(
-        network_ip + router_port + "/personalScheduleFunc"
+        network_ip + router_port + "/personalScheduleFunc",
+        {
+          params: {
+            company_id: "056",
+            employee_id: "9565656565656",
+            startDate: "2024/08/25",
+            // endDate: "2024/08/31",
+            endDate: "2024/09/01",
+          },
+        }
       );
-      setClients(response.data);
-      // console.log("response.data: ", response.data); // TESTING
-      // console.log("response: ", response); // TESTING
-      console.log(response.data);
+      // use response.data
+
+      // Now group the jobs by date:
+      const groupedJobs = {}; // STORES DICTIONARY WITH KEY: YYYY/MM/DD
+      response.data.forEach((job) => {
+        // Extract date from jobDate_job_id (YYYYMMDD_part)
+        const jobDate = job.jobDate_job_id.split("_")[0];
+        const formattedDate = `${jobDate.slice(0, 4)}/${jobDate.slice(
+          4,
+          6
+        )}/${jobDate.slice(6, 8)}`; // Format to YYYY/MM/DD
+
+        // Group jobs by date
+        if (!groupedJobs[formattedDate]) {
+          groupedJobs[formattedDate] = [];
+        }
+        groupedJobs[formattedDate].push(job);
+      });
+
+      setGroupedJobsData(groupedJobs); // SET AS STATE
+      setIsLoading(false); // UNLOAD LOADING ICON
     } catch (error) {
-      console.error("Error fetching clients:", error);
+      console.error(
+        "FROM ScreenPersonalSchedule > fetchAndGroupJobs(), ERROR GETTING JOBS:",
+        error
+      );
     }
   };
 
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light" />
+  try {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light" />
+        {/* GRADIENT BACKGROUND */}
+        <View style={styles.gradientContainer}>
+          <BackgroundGradient />
+        </View>
+        <View style={styles.testerBg}></View>
+        {/* MAIN CONTENT */}
+        <View style={styles.contentContainer}>
+          {/* BOTH ELEMS AND EDIT BUTTONS: FLEX: 1 */}
+          <NavBar screenTitle={"Schedule"} />
+          <DateSelector date1={testerDate1} date2={testerDate2}></DateSelector>
+          {/* MAIN BODY */}
+          <View style={styles.containerAllJobs}>
+            {/* RENDER DYNAMIC COMPONENTS */}
+            {isLoading ? (
+              <ActivityIndicator size="large" color="#D9AC6E" />
+            ) : (
+              Object.keys(groupedJobsData).map((date) => (
+                <View key={date} style={styles.containerDate}>
+                  <DateHeader date={date} />
 
-      {/* GRADIENT BACKGROUND */}
-      <View style={styles.gradientContainer}>
-        {/* <RadialGradient
-          x="50%"
-          y="50%"
-          rx="50%"
-          ry="50%"
-          colorList={colorList}
-          style={styles.gradientBg}
-        /> */}
-        <BackgroundGradient />
+                  {groupedJobsData[date].map((job, index) => (
+                    <View key={index} style={styles.containerJob}>
+                      <JobEntry jobName={job.jobName} startTime={job.jobTime} />
+                    </View>
+                  ))}
+                </View>
+              ))
+            )}
+          </View>
+          <EditButtons />
+        </View>
       </View>
-      <View style={styles.testerBg}></View>
-
-      {/* MAIN CONTENT */}
-      <View style={styles.contentContainer}>
-        <NavBar screenTitle={"Schedule"}></NavBar>
-
-        <DateSelector date1={testerDate1} date2={testerDate2}></DateSelector>
-        <View style={styles.goldLine}></View>
-
-        <DateHeader date={testerDate1}></DateHeader>
-        <JobEntry jobName={"JOB1"} startTime={"56 AM"}></JobEntry>
-
-        <DateHeader date={"2024/08/24"}></DateHeader>
-        <JobEntry
-          jobName={
-            "JOB2 when the text is too long MILKY RAY MILKY RAY MILKY RAY MILKY RAY MILKY RAY MILKY RAY MILKY RAY "
-          }
-          startTime={"56 AM"}
-        ></JobEntry>
-        <JobEntry jobName={"JOB3"} startTime={"56 AM"}></JobEntry>
-
-        <EditButtons></EditButtons>
-      </View>
-    </View>
-  );
+    );
+  } catch (error) {
+    console.log("FROM ScreenPersonalSchedule.js, ERROR:", error);
+  }
 };
-
-const colorList = [
-  { offset: "0%", color: "#131313", opacity: "1" },
-  { offset: "66.9%", color: "#2D2C2C", opacity: "1" },
-  { offset: "100%", color: "#131313", opacity: "1" },
-];
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     position: "relative", // Allows the content to be positioned over the gradient
     // backgroundColor: "transparent", // This makes the gradient not appear
+    width: "100%",
+    height: "100%",
+    maxHeight: Dimensions.get("window").height,
+  },
+  containerAllJobs: {
+    flex: 7,
+    width: "100%",
+    alignItems: "center",
+    overflow: "scroll",
+  },
+  containerDate: {
+    width: "100%",
+  },
+  containerJob: {
+    width: "100%",
+    alignItems: "center",
   },
   gradientContainer: {
     ...StyleSheet.absoluteFillObject,
